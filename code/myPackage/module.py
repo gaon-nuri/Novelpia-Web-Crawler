8a47a275c33d906737a8854a3c808a338629e7fd
gaon-nuri <113691868+gaon-nuri@users.noreply.github.com>
2024-08-07 00:49:51 +0900

    출력, 환경 변수, 로그인 키, 파일 갱신 관련 함수 추가. 회차 목록 요청 시 로그인 키 사용하게 수정.

--- code/myPackage/module.py
+++ code/myPackage/module.py
@@ -1,4 +1,5 @@
 import os  # os.environ[]
+import time
 from pathlib import Path
 
 import requests
@@ -6,6 +7,48 @@ from bs4 import BeautifulSoup
 
 # Windows Chrome User-Agent String
 ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'
+
+
+def print_with_new_line(*msg: object) -> None:
+	print()
+	print(*msg)
+
+
+def get_env_var(key: str) -> str | None:
+	"""
+	입력받은 이름의 환경 변수를 찾아서 값을 반환하는 함수
+
+	:param key: 이름
+	:return: 값
+	"""
+	# 환경 변수 有, 반환
+	try:
+		return os.environ[key]
+
+	# 환경 변수 無, 빈 문자열 반환
+	except KeyError as ke:
+		print_with_new_line(f"예외 발생: {ke = }")
+		print(f"환경 변수 '{key}' 을 찾지 못했어요.")
+		return None
+
+
+def add_login_key(headers: dict[str: str]) -> (str, dict):
+	"""
+	입력받은 헤더에 로그인 키를 추가해서 반환하는 함수
+
+	:param headers: 로그인 키를 추가할 헤더
+	:return: 추가한 로그인 키, 새 헤더
+	"""
+	login_key: str = get_env_var("LOGINKEY")
+
+	# 로그인 키 유무 검사
+	assert login_key is not None
+
+	# 로그인 키 有
+	cookie: str = "LOGINKEY=" + login_key
+	headers["Cookie"] = cookie
+
+	return login_key, headers
 
 
 def ask_for_number(number_type: str) -> int:
@@ -18,7 +61,7 @@ def ask_for_number(number_type: str) -> int:
 	while True:
 		number_string: str = str(int(ask_for_string("[입력] " + number_type, int)))  # 01, 001,... > 1
 		is_valid_num: bool = ask_for_permission(f"[확인] {number_type}가 {number_string}인가요?")[1]
-		number: int = int(number_string)
+		number = int(number_string)
 
 		# 유효한 소설 번호 (자연수)
 		if is_valid_num and number >= 0:
@@ -47,7 +90,7 @@ def ask_for_string(prompt: str = "입력", type: type = str) -> str:
 
 		# 해당 형식으로 변환할 수 없는 문자열
 		except ValueError as ve:
-			print(f"예외 발생: {ve}")
+			print(f"예외 발생: {ve = }")
 			return False
 
 	# 유효한 형식의 문자열을 입력받을 때까지 반복
@@ -63,8 +106,7 @@ def ask_for_string(prompt: str = "입력", type: type = str) -> str:
 
 		# 무효한 문자열
 		if not is_valid_string:
-			print()
-			print("[오류] 잘못된 입력이에요. 이전 단계로 돌아갈게요.")
+			print_with_new_line("[오류] 잘못된 입력이에요. 이전 단계로 돌아갈게요.")
 			continue
 		return answer_str
 
@@ -113,69 +155,89 @@ def assure_path_exists(path_to_assure: Path) -> None:
 			break
 
 
-def set_cookie(key: str, val: str = "") -> str:
-	"""
-	입력받은 쿠키를 '이름=값;' 형식으로 변환하여 반환하는 함수
-
-	:param key: 쿠키 이름
-	:param val: 쿠키 값
-	:return: "이름=값;" 형식의 문자열
-	"""
-	# 쿠키 값 有, 사용
-	if val != "":
-		return key + "=" + val + ";"
-	else:
-		# 환경 변수 有, 사용
-		try:
-			return key + "=" + os.environ[key] + ";"
-
-		# 환경 변수 無, 빈 문자열 반환
-		except KeyError as ke:
-			print()
-			print(f"예외 발생: {ke = }")
-			print(f"환경 변수 '{key}' 을 찾지 못했어요.")
-			return ''
-
-
 def get_novel_main_page(url: str) -> str:
 	"""
-	서버에 소설의 Main Webpage 를 요청하고, 성공 시 HTML 응답을 반환하는 함수
+	서버에 소설의 메인 페이지 를 요청하고, 성공 시 HTML 응답을 반환하는 함수
 
-	:param url: 소설 Main Webpage URL
-	:return: 소설 Main Webpage HTML
+	:param url: 소설 메인 페이지 URL
+	:return: 소설 메인 페이지 HTML
 	"""
 	headers: dict = {'User-Agent': ua}
 
-	# 소설 Main Webpage 의 HTML 문서를 요청
+	# 소설 메인 페이지의 HTML 문서를 요청
 	response = requests.get(url=url, headers=headers)  # response: <Response [200]>
 	html: str = response.text
+
+	assert html != "", "노벨피아 서버 터짐"
+
 	return html
 
 
-def get_ep_list(code: str, sort: str = "DOWN", page: int = 1) -> str:
+def open_file_if_none(file_name: Path) -> None:
+	assure_path_exists(file_name)
+
+	# 기존 파일 유무 확인, 있으면 갱신 여부 질문
+	is_old_file_exist: bool = file_name.exists()
+
+	# 기존 파일 有
+	if is_old_file_exist:
+		mtime: str = time.ctime(file_name.stat().st_mtime)
+		question: str = f"[확인] {mtime} 에 수정된 파일이 있어요. 덮어 쓸까요?"
+
+		# 사용자에게 덮어쓸 것인지 질문
+		asked_overwrite_file, can_overwrite_file = ask_for_permission(question)
+
+		# 덮어쓸 경우
+		if can_overwrite_file:
+			print_with_new_line("[알림]", file_name, "파일에 덮어 썼어요.")  # 기존 파일 有, 덮어쓰기
+			print()
+
+		# 기존 파일을 유지, 덮어쓰지 않고 종료
+		else:
+			exit("[알림] 기존 파일이 있으니 파일 생성은 건너 뛸게요.")
+
+	# 기존 파일 無, 새로 내려받기
+	else:
+		print_with_new_line("[알림]", file_name, "에 새로 만들었어요.")
+
+
+def get_ep_list(code: str, sort: str = "DOWN", page: int = 1, use_login_key: bool = False) -> str:
 	"""
 	서버에 회차 목록을 요청하고, 성공 시 HTML 응답을 반환하는 함수
 
 	:param code: 소설 번호
 	:param sort: "DOWN", 첫화부터 / "UP", 최신화부터
-	:param page: 요청할 페이지 번호 (자연수)
+	:param page: 요청할 페이지 번호
+	:param use_login_key: 로그인 키 사용 여부 (링크 추출용)
 	:return: 게시일 순으로 정렬된 회차 목록의 한 페이지 (HTML)
 	"""
 	# Chrome DevTools 에서 복사한 POST 요청 URL 및 양식 데이터
 	req_url: str = "https://novelpia.com/proc/episode_list"
-	form_data: dict = {"novel_no": code, "sort": sort, "page": page - 1}  # 1페이지 -> "page": 0, ...
-	cookie: str = set_cookie("LOGINKEY")
+	form_data: dict = {"novel_no": code, "sort": sort, "page": page - 1}  # 1페이지 -> page = 0, ...
+	headers: dict[str: str] = {'User-Agent': ua}
 
-	headers: dict[str: str] = {'User-Agent': ua, 'Cookie': cookie}
+	# 로그인 O
+	if use_login_key:
+		login_key_added, headers_with_login_key = add_login_key(headers)
 
-	# 게시일 순으로 정렬된 회차 목록 요청
-	response = requests.post(url=req_url, data=form_data, headers=headers)  # response: <Response [200]>
-	html = response.text
+		# 게시일 순으로 정렬된 회차 목록 요청
+		response = requests.post(url=req_url, data=form_data, headers=headers_with_login_key)  # response: <Response [200]>
 
+	# 로그인 X
+	else:
+		response = requests.post(url=req_url, data=form_data, headers=headers)  # response: <Response [200]>
+
+	html: str = response.text
 	return html
 
 
 def has_prologue(novel_code: str) -> bool:
+	"""
+	소설의 회차 목록에서 프롤로그의 유무를 반환하는 함수
+
+	:param novel_code:
+	:return: 프롤로그 유무
+	"""
 	fst_page_ep_list_html: str = get_ep_list(novel_code, sort="DOWN", page=1)
 	fst_ep_list_soup = BeautifulSoup(fst_page_ep_list_html, "html.parser")
 	fst_page_ep_list = fst_ep_list_soup.select("tr.ep_style5 td.font12")
