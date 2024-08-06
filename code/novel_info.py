6de1b2e160313a7a9873070c62ff8630820371e4
gaon-nuri <113691868+gaon-nuri@users.noreply.github.com>
2024-08-07 00:52:05 +0900

    Markdown 변환과 파일 생성 코드 분리.

--- code/novel_info.py
+++ code/novel_info.py
@@ -37,7 +37,7 @@ def extract_metadata(main_page_html: str) -> dict:
     :param main_page_html: 소설 Metadata 를 추출할 HTML
     :return: 추출한 Metadata 가 담긴 Dict
     """
-    metadata_dic: dict = {
+    info_dic: dict = {
         "title": "",
         "author": "",
         "novel_page_url": "",
@@ -66,9 +66,6 @@ def extract_metadata(main_page_html: str) -> dict:
         },
         "synopsis": ""
     }
-    metadata_dic.setdefault("fstUploadDate", "0000-00-00")
-    metadata_dic.setdefault("lstUploadDate", "0000-00-00")
-
     soup = BeautifulSoup(main_page_html, "html.parser")
 
     # 제목 추출
@@ -78,36 +75,37 @@ def extract_metadata(main_page_html: str) -> dict:
     # tag: <meta content="https://novelpia.com/novel/1" property="og:url"/>
     meta_url_tag = soup.select_one("meta[property='og:url']")
     novel_page_url: str = meta_url_tag.get("content")
-    metadata_dic["novel_page_url"] = novel_page_url
+    info_dic["novel_page_url"] = novel_page_url
 
     # 소설 번호 추출
     novel_code: str = urlparse(novel_page_url).path.split("/")[-1]  # "https://novelpia.com/novel/1" > "1"
 
     # 소설 서비스 상태 확인
-    # novel_info = soup.select_one(".epnew-novel-info")
     try:
         # 제목 추출, 검사, 등록
         if title == soup.select_one("div.epnew-novel-title").string:  # '창작물 속으로'
-            metadata_dic["title"] = title
+            info_dic["title"] = title
 
+    # 서비스 상태 비정상, 소설 정보를 받지 못함
     except AttributeError as ae:
-        print()
-        print(f"[오류] 예외 발생: {ae}")
+        print_with_new_line(f"[오류] 예외 발생: {ae = }")
 
+        # 알림 창 추출
         try:
-            # if not novel_info is None:
             msg_tag = soup.select_one("#alert_modal .mg-b-5")
+
+        # 알림 창이 나오지 않음
+        except AttributeError as ae:
+            print(f"예외 발생: {ae = }")
+
+        # 알림 메시지 추출
+        else:
             msg: str = msg_tag.text
             """
             0: 잘못된 소설 번호 입니다. (제목, 줄거리 無)
             4: 삭제된 소설 입니다. (제목 有, 줄거리 無)
             200000: 잘못된 접근입니다. (제목 有, 줄거리 無)
             """
-
-        except AttributeError as ae:
-            print(f"예외 발생: {ae}")
-
-        else:
             if msg == "잘못된 소설 번호 입니다.":
                 pass
             elif msg == "잘못된 접근입니다.":
@@ -115,44 +113,44 @@ def extract_metadata(main_page_html: str) -> dict:
             elif msg == "삭제된 소설 입니다.":
                 msg = f"<{title}> 은(는) {msg}"
 
-            # 오류 메시지를 출력하며 종료
+            # 오류 메시지 출력 후 종료
             print()
             exit_code = f"[노벨피아] {msg}"
             exit(exit_code)
 
-    # 소설 서비스 상태 정상, 계속 진행
+    # 서비스 상태 정상, 계속 진행
     else:
         # 작가명 추출
-        metadata_dic["author"]: str = soup.select_one("a.writer-name").string.strip()  # '제울'
+        info_dic["author"]: str = soup.select_one("a.writer-name").string.strip()  # '제울'
 
         print()
-        print(f"[알림] {metadata_dic['author']} 작가의 <{metadata_dic['title']}>은 정상적으로 서비스 중인 소설이에요.")
+        print(f"[알림] {info_dic['author']} 작가의 <{info_dic['title']}>은 정상적으로 서비스 중인 소설이에요.")
 
         # 연재 유형(자유/PLUS), 청불/독점작/챌린지/연중(및 지연)/완결 여부
         badge_list: list[str] = [badge.text for badge in soup.select(".in-badge span")]
 
         for flag in badge_list:
-            if flag in metadata_dic["badge_dic"].keys():
-                metadata_dic["badge_dic"][flag] = True
+            if flag in info_dic["badge_dic"].keys():
+                info_dic["badge_dic"][flag] = True
 
         # 해시태그 목록 추출 (최소 2개 - 중복 포함 4개)
         tag_set = soup.select("p.writer-tag span.tag")
 
         # PC, 모바일 중복 해시태그 제거
         indices: int = int(len(tag_set) / 2)  # 3
-        metadata_dic["tagList"]: list[str] = [i.string for i in tag_set[:indices]]  # ['판타지','#현대', '#하렘']
+        info_dic["tagList"]: list[str] = [i.string for i in tag_set[:indices]]  # ['판타지','#현대', '#하렘']
 
         # 조회, 추천수 추출
         ep_stat_list: list[str] = [i.string.replace(",", "") for i in soup.select("div.counter-line-a span")]
-        metadata_dic["ep_stat_dic"]["view"] = int(ep_stat_list[1])  # 83,050,765 > 83050765
-        metadata_dic["ep_stat_dic"]["recommend"] = int(ep_stat_list[3])  # 4,540,540 > 4540540
+        info_dic["ep_stat_dic"]["view"] = int(ep_stat_list[1])  # 83,050,765 > 83050765
+        info_dic["ep_stat_dic"]["recommend"] = int(ep_stat_list[3])  # 4,540,540 > 4540540
 
         # 인생픽 순위 추출
         sole_pick_rank: str = soup.select(".counter-line-b span")[1].text
 
         # 인생픽 순위 공개 중, 속성 추가
         if sole_pick_rank[-1] == "위":  # 40위
-            metadata_dic["solePick"] = "\n인생픽: " + sole_pick_rank  # 인생픽: 40위 (Markdown 속성 문법)
+            info_dic["solePick"] = "\n인생픽: " + sole_pick_rank  # 인생픽: 40위 (Markdown 속성 문법)
 
         # 비공개 상태일 시 넘어가기
         elif sole_pick_rank == "공개전":
@@ -163,104 +161,86 @@ def extract_metadata(main_page_html: str) -> dict:
         user_stat_li[2] = user_stat_li[2].replace("회차", "")  # 2538회차 > 2538
         user_stat_li_int: list[int] = [int(user_stat) for user_stat in user_stat_li][::-1]
 
-        metadata_dic["user_stat_dic"] = dict(zip(metadata_dic["user_stat_dic"].keys(), user_stat_li_int))
+        info_dic["user_stat_dic"] = dict(zip(info_dic["user_stat_dic"].keys(), user_stat_li_int))
 
         # 프롤로그 존재 시 회차 수 1 가산
         # EP.0 ~ EP.10 이면 10회차로 나오니 총 회차 수는 여기에 1을 더해야 함
         if has_prologue(novel_code):
-            metadata_dic["user_stat_dic"]["ep"] += 1
+            info_dic["user_stat_dic"]["ep"] += 1
 
         # 줄거리 추출
-        metadata_dic["synopsis"]: str = soup.select_one(".synopsis").text
+        info_dic["synopsis"]: str = soup.select_one(".synopsis").text
 
         # 공개 일자 추출
-        metadata_dic["fstUploadDate"] = get_upload_date(novel_code, "DOWN")
-        metadata_dic["lstUploadDate"] = get_upload_date(novel_code, "UP")
+        info_dic["fstUploadDate"] = get_upload_date(novel_code, "DOWN")
+        info_dic["lstUploadDate"] = get_upload_date(novel_code, "UP")
 
-    return metadata_dic
+    return info_dic
 
 
-def write_markdown(metadata_dic: dict, novel_markdown_name: Path) -> None:
+def convert_to_markdown(info_dic: dict) -> str:
     """
-    Markdown 파일을 새로 열고 추출한 소설 정보를 쓰는 함수
+    추출한 소설 정보를 받아 Markdown 형식으로 변환하는 함수
 
-    :param metadata_dic: 파일에 쓸 Metadata 가 담긴 Dict
-    :param novel_markdown_name: 생성할 파일의 경로
+    :param info_dic: 추출한 소설 정보
+    """
+    print(f"[알림] {info_dic['title']}.md 파일에 '유입 경로' 속성을 추가했어요. Obsidian 으로 직접 수정해 주세요.")
+
+    # 태그의 각 줄 앞에 '-'를 붙여서 Markdown list (Obsidian 태그) 형식으로 변환
+    tag_ul: str = "".join(f"\n  - {tag.lstrip('#')}" for tag in info_dic["tagList"])
+    """
+    tags:
+      - 판타지
+      - 중세
+    """
+    # 줄거리의 각 줄 앞에 '>'를 붙여서 Markdown Callout 블록으로 변환
+    synopsis_lines: list[str] = info_dic["synopsis"].splitlines()
+    synopsis_callout: str = "> [!TLDR] 시놉시스\n> "
+
+    if len(synopsis_lines) > 1:
+        synopsis_callout += "\n> ".join(line for line in synopsis_lines)
+    else:
+        synopsis_callout += synopsis_lines[0]
+
+    """
+    > [!TLDR] 시놉시스
+    > 몰살 엔딩 보려고 세상을 멸망시켰다.
+    > 
+    > 그리고 게임 속에 빙의당했다.
+    > - ai로 제작한 이미지입니다.
+    > - 상업적 이용이 가능합니다.
     """
 
-    # Markdown 폴더 확보하기
-    assure_path_exists(novel_markdown_name)
+    # 연재 지연/중단의 경우 '연중(각)'으로 표시
+    on_hiatus: bool = (info_dic["badge_dic"]["연재지연"] != info_dic["badge_dic"]["연재중단"])
 
-    # Markdown 파일 열기
-    with (open(novel_markdown_name, "w") as novelMarkDownFile):
-        aliases: str = ""
-        aliases_q: str = f"[확인] {metadata_dic['title']}.md 파일에 '별칭(aliases)' 속성을 추가할까요?"
-        asked_add_alias, will_add_alias = ask_for_permission(aliases_q)
-
-        # 별칭을 추가할 경우
-        if will_add_alias:
-            aliases = "\naliases: \n  - (직접 적어 주세요)"  # Markdown 속성 문법
-            print()
-            print(f"[알림] {metadata_dic['title']}.md 파일에 '별칭(aliases)' 속성을 추가했어요. Obsidian 으로 직접 수정해 주세요.")
-            print()
-
-        print(f"[알림] {metadata_dic['title']}.md 파일에 '유입 경로' 속성을 추가했어요. Obsidian 으로 직접 수정해 주세요.")
-
-        # 태그의 각 줄 앞에 '-'를 붙여서 Markdown list (Obsidian 태그) 형식으로 변환
-        tag_ul: str = "".join(f"\n  - {tag.lstrip('#')}" for tag in metadata_dic["tagList"])
-        """
-        tags:
-          - 판타지
-          - 중세
-        """
-        # 줄거리의 각 줄 앞에 '>'를 붙여서 Markdown Callout 블록으로 변환
-        synopsis_lines: list[str] = metadata_dic["synopsis"].splitlines()
-        synopsis_callout: str = "> [!TLDR] 시놉시스\n> "
-
-        if len(synopsis_lines) > 1:
-            synopsis_callout += "\n> ".join(line for line in synopsis_lines)
-        else:
-            synopsis_callout += synopsis_lines[0]
-
-        """
-        > [!TLDR] 시놉시스
-        > 몰살 엔딩 보려고 세상을 멸망시켰다.
-        > 
-        > 그리고 게임 속에 빙의당했다.
-        > - ai로 제작한 이미지입니다.
-        > - 상업적 이용이 가능합니다.
-        """
-
-        # 연재 지연/중단의 경우 '연중(각)'으로 표시
-        on_hiatus: bool = (metadata_dic["badge_dic"]["연재지연"] != metadata_dic["badge_dic"]["연재중단"])
-
-        # 최종 Markdown
-        novel_markdown_content: str = f"""---{aliases}
-작가명: {metadata_dic["author"]}
-링크: {metadata_dic["novel_page_url"]}
-유입 경로: (직접 적어 주세요) {metadata_dic["solePick"]}
+    # 최종 Markdown
+    novel_markdown_content: str = f"""---
+aliases:
+  - 
+작가명: {info_dic["author"]}
+링크: {info_dic["novel_page_url"]}
+유입 경로: (직접 적어 주세요) {info_dic["solePick"]}
 tags:{tag_ul}
-공개: {metadata_dic["fstUploadDate"]}
-갱신: {metadata_dic["lstUploadDate"]}
+공개: {info_dic.setdefault("fstUploadDate", "0000-00-00")}
+갱신: {info_dic.setdefault("lstUploadDate", "0000-00-00")}
 완독: 0000-00-00
-완결: {metadata_dic["badge_dic"]["완결"]}
+완결: {info_dic["badge_dic"]["완결"]}
 연중(각): {on_hiatus}
-성인: {metadata_dic["badge_dic"]["19"]}
-무료: {metadata_dic["badge_dic"]["자유"]}
-독점: {metadata_dic["badge_dic"]["독점"]}
-챌린지: {metadata_dic["badge_dic"]["챌린지"]}
-회차: {metadata_dic["user_stat_dic"]["ep"]}
-알람: {metadata_dic["user_stat_dic"]["alarm"]}
-선호: {metadata_dic["user_stat_dic"]["prefer"]}
-추천: {metadata_dic["ep_stat_dic"]["recommend"]}
-조회: {metadata_dic["ep_stat_dic"]["view"]}
+성인: {info_dic["badge_dic"]["19"]}
+무료: {info_dic["badge_dic"]["자유"]}
+독점: {info_dic["badge_dic"]["독점"]}
+챌린지: {info_dic["badge_dic"]["챌린지"]}
+회차: {info_dic["user_stat_dic"]["ep"]}
+알람: {info_dic["user_stat_dic"]["alarm"]}
+선호: {info_dic["user_stat_dic"]["prefer"]}
+추천: {info_dic["ep_stat_dic"]["recommend"]}
+조회: {info_dic["ep_stat_dic"]["view"]}
 ---
-{synopsis_callout}"""
+{synopsis_callout}
+"""
 
-        # MarkDown 파일 쓰기
-        novelMarkDownFile.write(novel_markdown_content)
-        print()
-        print(novel_markdown_name, "작성함.")
+    return novel_markdown_content
 
 
 def main() -> None:
@@ -276,8 +256,7 @@ def main() -> None:
     # 환경 변수 無, 기본 값으로
     except KeyError as ke:
         novel_markdown_dir = novel_dir / "markdown"
-        print()
-        print(f"예외 발생: {ke = }")
+        print_with_new_line(f"`예외 발생: {ke = }")
         print(f"환경 변수 '{env_var_name}' 을 찾지 못했어요. Markdown 파일은 {novel_markdown_dir} 에 쓸게요.")
 
     novel_code: str = str(ask_for_number("소설 번호"))
@@ -291,8 +270,17 @@ def main() -> None:
     # ../novel/markdown/제목.md
     novel_markdown_name: Path = (novel_markdown_dir / metadata_dic['title']).with_suffix(".md")
 
-    # Markdown 파일 생성하기
-    write_markdown(metadata_dic, novel_markdown_name)
+    # Markdown 형식으로 변환하기
+    novel_markdown_content = convert_to_markdown(metadata_dic)
+
+    # Markdown 폴더 확보하기
+    assure_path_exists(novel_markdown_name)
+
+    # Markdown 파일 새로 쓰기
+    with (open(novel_markdown_name, "w") as novelMarkDownFile):
+        novelMarkDownFile.write(novel_markdown_content)
+        print()
+        print(novel_markdown_name, "작성함.")
 
 
 if __name__ == "__main__":
