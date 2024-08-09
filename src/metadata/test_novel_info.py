261975d64bb8b7930e3f774a28abfffa46b52022
gaon-nuri <113691868+gaon-nuri@users.noreply.github.com>
2024-08-09 09:53:08 +0900

    ends_with_vowel() 테스트 추가. chk_up_status() 함수 추가. 코드 구조 대폭 수정.
    Signed-off-by: gaon-nuri <113691868+gaon-nuri@users.noreply.github.com>

--- src/metadata/test_novel_info.py
+++ src/metadata/test_novel_info.py
@@ -1,45 +1,60 @@
 import unittest
 
-from novel_info import *
+from src.common.test_module import total_novel_cnt
+from src.metadata.novel_info import *
 
 
-class TestGetFstUploadDate(unittest.TestCase):
-    def test_no_ep(self):
-        for num in range(50):
-            code = str(num)
-            with self.subTest(code=code):
-                self.assertIsNone(get_ep_up_date(code))
-        # code: str = "247416"  #
-        # fst_up_date: str = "2023-12-11"
-        # self.assertEqual(get_ep_up_date(code), fst_up_date)
+class GetFinalJamo(unittest.TestCase):
+    def test_final_jamo(self):
+        kor_str_vowel_end: str = "창작물 속으로"
+        kor_str_cons_end: str = "매도당하고 싶은 엘프님"
 
+        is_fin_vowel: bool = ends_with_vowel(kor_str_vowel_end)
+        is_fin_cons: bool = ends_with_vowel(kor_str_cons_end)
+
+        self.assertTrue(is_fin_vowel and not is_fin_cons)
+
+
+class GetUploadDate(unittest.TestCase):
     def test_single_ep(self):
         code: str = "124146"  # <연중용 나데나데 소설>
-        fst_up_date: str = "2023-12-13"
-        self.assertEqual(get_ep_up_date(code), fst_up_date)
+        up_date: str = "2023-12-13"
+
+        self.assertEqual(get_ep_up_date(code), up_date)
 
     def test_multiple_ep(self):
         code: str = "247416"  # <숨겨진 흑막이 되었다>
         fst_up_date: str = "2023-12-11"
-        self.assertEqual(get_ep_up_date(code), fst_up_date)
+        lst_up_date: str = "2024-04-18"
 
+        fst_up_date_match: bool = get_ep_up_date(code, "DOWN") == fst_up_date
+        lst_up_date_match: bool = get_ep_up_date(code, "UP") == lst_up_date
 
-class TestGetLstUploadDate(unittest.TestCase):
+        self.assertTrue(fst_up_date_match and lst_up_date_match)
+
     def test_no_ep(self):
-        self.assertEqual(get_ep_up_date("247416", "DOWN"), "2023-12-11")
+        code: str = "2"  # <건물주 아들>, 최초의 삭제된 소설. 동명의 9번 소설의 습작?
 
-    def test_single_ep(self):
-        self.assertEqual(get_ep_up_date("247416", "DOWN"), "2023-12-11")
-
-    def test_multiple_ep(self):
-        self.assertEqual(get_ep_up_date("247416", "UP"), "2024-04-18")
+        self.assertIsNone(get_ep_up_date(code))
 
 
-class TestConvertToMarkdown(unittest.TestCase):
+class CntNoEpNovel(unittest.TestCase):
+    def test_cnt_no_ep_novel(self):
+        """
+        노벨피아 소설 중 작성된 회차가 없는 작품을 세는 테스트
+        :return: 회차가 없으면 성공, 있으면 실패
+        """
+        for num in range(total_novel_cnt):
+            code = str(num)
+            with self.subTest(code=code):
+                self.assertIsNone(get_ep_up_date(code))
+
+
+class ConvertToMarkdown(unittest.TestCase):
     def test_convert_to_markdown(self):
         """
-        소설의 Metadata 가 담긴 Dict 를 Markdown 형식의 문자열로 변환하는 테스트
-        :return: str
+        소설 정보가 담긴 Dict 를 Markdown 형식의 문자열로 변환하는 테스트
+        :return: Markdown 형식의 소설 정보
         """
         info_dic: dict = {
             'title': '숨겨진 흑막이 되었다',
@@ -95,9 +110,47 @@ tags:
         self.assertEqual(convert_to_markdown(info_dic), formatted_md)
 
 
-class TestExtractNovelInfo(unittest.TestCase):
-    def test_extract_novel_info(self):
-        self.fail()
+def chk_up_status(novel_code: str) -> str:
+    """
+    소설의 연재 상태를 추출하여 반환하는 함수
+
+    :param novel_code: 소설 번호
+    :return: '완결', '연재중단', '연재지연', '연재중' 중 택 1
+    """
+    url: str = f"https://novelpia.com/novel/{novel_code}"
+    html: str = get_novel_main_page(url)
+
+    info_dic: dict = extract_novel_info(html)
+    flag_dic: dict = info_dic["badge_dic"]
+    
+    for key in ["완결", "연재지연", "연재중단"]
+        if flag_dic[key]:
+            return key
+    
+    return "연재중"
+
+
+class GetNovelStatus(unittest.TestCase):
+    def test_get_novel_status(self):
+        code_on_hiatus: str = "10"  # <미대오빠의 여사친들> - 최초의 연중작
+        code_completed: str = "1"  # <S드립을 잘 치는 여사친> - 최초의 완결작
+        code_ongoing: str = "610"  # <창작물 속으로> - 연재중 (24.8.8 기준)
+
+        on_hiatus: bool = chk_up_status(code_on_hiatus)
+        completed: bool = chk_up_status(code_completed)
+        ongoing: bool = chk_up_status(code_ongoing)
+
+        self.assertTrue(on_hiatus and not completed or ongoing)
+
+
+class CntNovelOnHiatus(unittest.TestCase):
+    def test_cnt_novel_on_hiatus(self):
+        for num in range(total_novel_cnt):
+            code = str(num)
+            with self.subTest(code=code):
+                up_status: str = chk_up_status(code)
+                is_on_hiatus: bool = chk_up_status(code) == "연재지연" or "연재중단"
+                self.assertTrue(is_on_hiatus)
 
 
 if __name__ == '__main__':
