ebeea54ff8ea2a05ee507bbfc54264c32b8aaf09
gaon-nuri <113691868+gaon-nuri@users.noreply.github.com>
2024-08-15 01:47:23 +0900

    프로젝트 폴더 내 테스트 검색 기능 추가.
    Signed-off-by: gaon-nuri <113691868+gaon-nuri@users.noreply.github.com>

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+import unittest
+
+if __name__ == '__main__':
+    from os import getcwd
+    this_dir = getcwd()
+
+    suite = unittest.loader.defaultTestLoader.discover(this_dir)
+
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
