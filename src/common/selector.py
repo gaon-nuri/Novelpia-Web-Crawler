a3ee48bd1346f43473c5c88673a3d8c6bfa7b335
gaon-nuri <113691868+gaon-nuri@users.noreply.github.com>
2024-08-25 23:54:00 +0900

    CSS 선택자 원본을 selector.py로 이동.
    Signed-off-by: gaon-nuri <113691868+gaon-nuri@users.noreply.github.com>

---
+++ src/common/selector.py
@@ -0,0 +1,26 @@
+# src.metadata.novel_info
+NOVEL_ALERT_MSG_CSS: str = "#alert_modal .mg-b-5"
+NOVEL_INFO_CSS: str = "epnew-novel-info"
+NOVEL_TITLE_CSS: str = "div.epnew-novel-title"
+NOVEL_URL_CSS: str = "og:url"
+NOVEL_BADGE_CSS: str = "p.in-badge"
+NOVEL_WRITER_NAME_CSS: str = "writer-name"
+NOVEL_HASH_TAG_CSS: str = "p.writer-tag span.tag"
+NOVEL_STAT_CSS: str = ".counter-line-a span"
+NOVEL_SOLE_PICK_RANK_CSS: str = ".counter-line-b span"
+
+# src.user.mybook
+MY_BOOK_TITLE_CSS: str = "name_st"
+MY_BOOK_TABLE_ROW_CSS: str = "row"
+
+# src.common.episode
+EP_TABLE_CSS: str = "s_inv"
+EP_TAGS_CSS: str = "tr.ep_style5"
+EP_TYPES_CSS: str = "span.s_inv"
+EP_VIEW_CSS: str = "td.font12"
+EP_STATS_CSS: str = "div.ep_style2 font"
+EP_VIEW_COUNT_CSS: str = ".episode_count_view"
+EP_LETTER_COUNT_CSS: str = "ion-document-text"
+EP_COMMENT_COUNT_CSS: str = "ion-chatbox-working"
+EP_RECOMMEND_COUNT_CSS: str = "ion-thumbsup"
+EP_LINK_CSS: str = ".page-link"
