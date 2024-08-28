a1ec4dd4be4f1f799aea07f2decbbbb39a42c91e
gaon-nuri <113691868+gaon-nuri@users.noreply.github.com>
2024-08-28 10:42:51 +0900

    TestChkStrType 클래스를 test_userIO.py 로 분리.
    Signed-off-by: gaon-nuri <113691868+gaon-nuri@users.noreply.github.com>

---
+++ src/myTest/test_userIO.py
@@ -0,0 +1,22 @@
+"""사용자 입출력 기능 테스트"""
+from unittest import TestCase, main
+
+
+class TestChkStrType(TestCase):
+    def test_str_is_num(self):
+        num: str = "1"
+        num_in_tab: str = "\t12\t"
+
+        alnum: str = "a1"
+        tab_in_num: str = "1\t2"
+
+        from src.common.userIO import chk_str_type
+
+        is_num: bool = chk_str_type(num, int) and chk_str_type(num_in_tab, int)
+        is_not_num: bool = chk_str_type(alnum, int) or chk_str_type(tab_in_num, int)
+
+        self.assertTrue(is_num and not is_not_num)
+
+
+if __name__ == '__main__':
+    main()
