d70b641e32ae3e2a5c9aad82aa49bada04c6e53b
gaon-nuri <113691868+gaon-nuri@users.noreply.github.com>
2024-08-28 10:42:09 +0900

    Update test_module.py
    Signed-off-by: gaon-nuri <113691868+gaon-nuri@users.noreply.github.com>

--- src/myTest/test_module.py
+++ src/myTest/test_module.py
@@ -1,60 +1,56 @@
+"""공통 함수 테스트"""
 import shutil
 from os import environ
 from unittest import TestCase, main, skip
 
-from src.common.module import *
-
 
 class TestGetEnvVar(TestCase):
-    """
-    입력받은 이름의 환경 변수가 있는 경우와 없는 경우 모두 테스트
-    """
+    """입력받은 이름의 환경 변수가 있는 경우와 없는 경우 모두 테스트"""
+    from src.common.module import get_env_var_w_error
+
+    @classmethod
+    def get_env_var(cls, test_key: str):
+        """get_env_var_w_error 함수 주석 참고"""
+        return cls.get_env_var_w_error(test_key)
 
     @classmethod
     def setUpClass(cls):
+        """테스트 준비 메서드"""
         environ["test_key"] = "test_val"
 
     def test_key_found(self):
         test_key: str = "test_key"
-        with get_env_var_w_error(test_key) as (test_val, ke):
+
+        with self.get_env_var(test_key) as (test_val, ke):
             self.assertTrue(test_val == "test_val" and ke is None)
 
     def test_key_not_found(self):
         test_key: str = "fake_key"
-        with get_env_var_w_error(test_key) as (test_val, ke):
+        with self.get_env_var(test_key) as (test_val, ke):
             self.assertTrue(test_val is None and isinstance(ke, KeyError))
 
     @classmethod
     def tearDownClass(cls):
+        """테스트 뒷정리 메서드"""
         environ.pop("test_key")
 
 
 class AddLoginKey(TestCase):
     def test_add_login_key(self):
-        header: dict[str: str] = {"User-Agent": UA}
-        given_key, new_header = add_login_key(header)
+        from src.common.const import BASIC_HEADERS
+        from src.common.module import add_login_key
+
+        given_key, new_header = add_login_key(BASIC_HEADERS)
         real_key: str = new_header["Cookie"].split("=")[1]
 
         self.assertEqual(real_key, given_key)
 
 
-class TestChkStrType(TestCase):
-    def test_str_is_num(self):
-        num: str = "1"
-        num_in_tab: str = "\t12\t"
-
-        alnum: str = "a1"
-        tab_in_num: str = "1\t2"
-
-        from src.common.userIO import chk_str_type
-        is_num: bool = chk_str_type(num, int) and chk_str_type(num_in_tab, int)
-        is_not_num: bool = chk_str_type(alnum, int) or chk_str_type(tab_in_num, int)
-
-        self.assertTrue(is_num and not is_not_num)
-
-
 class TestAssurePathExists(TestCase):
     def test_any_path(self):
+        from pathlib import Path
+        from src.common.module import assure_path_exists
+
         test_path = Path("~/path/to/assure/file.ext").expanduser()
         assure_path_exists(test_path)
         parents = test_path.parents
@@ -73,10 +69,22 @@ class TestAssurePathExists(TestCase):
                     break
 
 
+def join_url(code: str):
+    """urljoin 함수 주석 참고"""
+    base = GetNovelMainPage.HOST
+    return GetNovelMainPage.urljoin(base, code)
+
+
 class GetNovelMainPage(TestCase):
+    from src.common.const import HOST
+    from urllib.parse import urljoin
+
     def test_valid_novel_code(self):
+        from src.common.module import get_novel_main_w_error
+
         code: str = "247416"
-        url: str = "https://novelpia.com/novel/" + code
+        url: str = join_url(code)
+
         with get_novel_main_w_error(url) as (html, connect_err):
             assert connect_err is None, f"{connect_err = }"
 
@@ -84,12 +92,13 @@ class GetNovelMainPage(TestCase):
 
     @skip
     def test_valid_novel_codes(self):
-        from src.common.const import TOTAL_NOVEL_COUNT
+        from src.common.const import ALL_NOVEL_COUNT
+        from src.common.module import get_novel_main_w_error
 
-        for num in range(1, TOTAL_NOVEL_COUNT):
+        for num in range(1, ALL_NOVEL_COUNT):
             code = str(num)
             with self.subTest(code=code):
-                url: str = "https://novelpia.com/novel/" + code
+                url: str = join_url(code)
                 with get_novel_main_w_error(url) as (html, connect_err):
                     assert connect_err is None, f"{connect_err = }"
 
