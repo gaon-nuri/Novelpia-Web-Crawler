95081694b24c07784b75ae3325d67039f4ce9166
gaon-nuri <113691868+gaon-nuri@users.noreply.github.com>
2024-11-08 12:53:17 +0900

    CSS 선택자를 여러 파일로 분리
    Signed-off-by: gaon-nuri <113691868+gaon-nuri@users.noreply.github.com>

---
+++ src/const/sel/css_ep.py
@@ -0,0 +1,14 @@
+"""CSS 선택자 모음"""
+################################################################################
+# src.func.episode
+################################################################################
+EP_TABLE_CSS: str = "s_inv"
+EP_TAGS_CSS: str = "tr.ep_style5"
+EP_TYPES_CSS: str = "span.s_inv"
+EP_VIEW_CSS: str = "td.font12"
+EP_STATS_CSS: str = "div.ep_style2 font"
+EP_VIEW_COUNT_CSS: str = ".episode_count_view"
+EP_LETTER_COUNT_CSS: str = "ion-document-text"
+EP_COMMENT_COUNT_CSS: str = "ion-chatbox-working"
+EP_RECOMMEND_COUNT_CSS: str = "ion-thumbsup"
+EP_LINK_CSS: str = ".page-link"
---
+++ src/const/sel/css_novel_info.py
@@ -0,0 +1,13 @@
+"""CSS 선택자 모음"""
+################################################################################
+# src.novel_info
+################################################################################
+NOVEL_ALERT_MSG_CSS: str = "#alert_modal .mg-b-5"
+NOVEL_INFO_CSS: str = "epnew-novel-info"
+NOVEL_TITLE_CSS: str = "div.epnew-novel-title"
+NOVEL_URL_CSS: str = "og:out_url"
+NOVEL_BADGE_CSS: str = "p.in-badge"
+NOVEL_WRITER_NAME_CSS: str = "writer-name"
+NOVEL_HASH_TAG_CSS: str = "p.writer-tag span.tag"
+NOVEL_STAT_CSS: str = ".counter-line-a span"
+NOVEL_SOLE_PICK_RANK_CSS: str = ".counter-line-b span"
