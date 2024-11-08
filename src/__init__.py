47532ded699c3f6041de06af67c1f7228873ba86
gaon-nuri <113691868+gaon-nuri@users.noreply.github.com>
2024-11-08 12:58:34 +0900

    로깅 도입 및 전역 로깅 설정 추가
    Signed-off-by: gaon-nuri <113691868+gaon-nuri@users.noreply.github.com>

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,20 @@
-import unittest
+"""프로젝트 전체용 코드"""
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.StreamHandler()
+    ]
+)
 
 if __name__ == '__main__':
     from os import getcwd
 
     this_dir = getcwd()
+
+    import unittest
 
     suite = unittest.loader.defaultTestLoader.discover(this_dir)
 
