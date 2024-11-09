b93942ff44f27c88c2a07b6d081acbaf7a0287ba
gaon-nuri <113691868+gaon-nuri@users.noreply.github.com>
2024-11-10 01:44:42 +0900

    파싱 관련 상수 분리
    Signed-off-by: gaon-nuri <113691868+gaon-nuri@users.noreply.github.com>

--- src/const/const.py
+++ src/const/const.py
@@ -1,48 +1,47 @@
-"""공용 상수 모음
-
-"""
+# novel_crawler/const/const.py
+"""공용 상수 모음"""
 from collections import namedtuple
 
-# Windows Chrome User-Agent String
-UA: str = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'
-BASIC_HEADERS: dict = {
-    "User-Agent": UA
-}
-
-PARSER: str = "html.parser"  # bs4 용 파이썬 내장 파서
-
-HOST: str = "https://novelpia.com"
-
-DEFAULT_TIME: str = "0000-00-00T00:00"
+################################################################################
+# 공통
+################################################################################
+PARSER: str = "page.parser"  # bs4 용 파이썬 내장 파서
+BASE_URL: str = "https://novelpia.com"
+BASE_TIME: str = "0000-00-00T00:00"
 
 ################################################################################
-# src.func.module
+# src.func.common
 ################################################################################
-LOGIN_KEY_NAME: str = "LOGINKEY"
+LOG_KEY_NAME: str = "LOGINKEY"
 
-POSTPOSITIONS_NAMED_TUPLE_CLS = namedtuple("KoPostpositions", "vowel consonant")
-POSTPOSITIONS_NAMED_TUPLE = POSTPOSITIONS_NAMED_TUPLE_CLS(("가", "를", "는", "야"), ("이", "을", "은", "아"))
-
-################################################################################
-# src.novel_info
-################################################################################
-HTML_TITLE_PREFIX: str = '노벨피아 - 웹소설로 꿈꾸는 세상! - '
-
-NOVEL_TYPES_NAMED_TUPLE_CLS = namedtuple("NovelTypes", "adult free exclusive challenge")
-NOVEL_STATUSES_NAMED_TUPLE_CLS = namedtuple("NovelStatuses", "ongoing deleted draft complete delayed hiatus")
-
-NOVEL_TYPES_NAMED_TUPLE = NOVEL_TYPES_NAMED_TUPLE_CLS("19", "자유", "독점", "챌린지")
-NOVEL_STATUSES_NAMED_TUPLE = NOVEL_STATUSES_NAMED_TUPLE_CLS("연재 중", "삭제", "연습작품", "완결", "연재지연", "연재중단")
-
-RANK_PLACE_HOLDER: str = "공개전"
+SUFFIX_NAMED_TU_CLS = namedtuple("KoSuffixes", "vowel cons")
+SUFFIX_NAMED_TU = SUFFIX_NAMED_TU_CLS(("가", "를", "는", "야"), ("이", "을", "은", "아"))
 
 ################################################################################
 # src.func.episode
 ################################################################################
-EP_TYPES_NAMED_TUPLE_CLS = namedtuple("EpTypes", "free plus adult")
-EP_TYPES_NAMED_TUPLE = EP_TYPES_NAMED_TUPLE_CLS("자유", "PLUS", "성인")
+EP_TYPES_NAMED_TU_CLS = namedtuple("EpTypes", "free plus adult")
+EP_TYPES_NAMED_TU = EP_TYPES_NAMED_TU_CLS("자유", "PLUS", "성인")
 
 ################################################################################
-# src.myTest
+# src.tests.*
 ################################################################################
-ALL_NOVEL_COUNT: int = 299487  # 노벨피아 소설 수 (2024-08-23 21:36 기준)
+ALL_NOVEL_CNT: int = 302663  # 노벨피아 소설 수 (2024-09-08 14:17 기준)
+
+################################################################################
+# src.novel_info
+################################################################################
+PAGE_TITLE_PREFIX: str = '노벨피아 - 웹소설로 꿈꾸는 세상! - '
+
+NOVEL_TYPES_NAMED_TU_CLS = namedtuple("NovelTypes", "adult free exclusive challenge")
+NOVEL_STATS_NAMED_TU_CLS = namedtuple("NovelStats", "ongoing deleted draft complete delayed hiatus")
+
+NOVEL_TYPES_NAMED_TU = NOVEL_TYPES_NAMED_TU_CLS("19", "자유", "독점", "챌린지")
+NOVEL_STATS_NAMED_TU = NOVEL_STATS_NAMED_TU_CLS("연재 중", "삭제", "연습작품", "완결", "연재 지연", "연재 중단")
+
+RANK_PLACE_HOLDER: str = "공개전"
+
+################################################################################
+# src.viewer
+################################################################################
+EP_CNT_IN_PAGE: int = 20  # 페이지 당 20회차씩
