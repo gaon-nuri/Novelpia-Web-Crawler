99c212f6671b7a392b33e5023874237fc456144c
rumor-thrower <87070008+rumor-thrower@users.noreply.github.com>
2025-08-23 01:28:04 +0900

    refactor(req): inline variable

--- src/user/shelf.py
+++ src/user/shelf.py
@@ -108,8 +108,7 @@ def toggle_novel_act(novel_code: str, stat_names: tuple[str, str]):
 
 def req_data_from_params(csrf_token: str, novel_code: str):
     if csrf_token:
-        req_data_dic: dict = {"novel_no": novel_code, "csrf": csrf_token, }
-        return req_data_dic
+        return {"novel_no": novel_code, "csrf": csrf_token, }
     else:
         err_msg = "CSRF 문자열을 입력받지 못했어요."
         raise log_and_return_error(NoValueError(err_msg))
