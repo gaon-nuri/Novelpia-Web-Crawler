a6e481e27e6d75d8b46b9eecdbb7677caccb93e1
rumor-thrower <87070008+rumor-thrower@users.noreply.github.com>
2025-08-23 02:26:02 +0900

    refactor(shelf): inline variables

--- src/user/shelf.py
+++ src/user/shelf.py
@@ -159,22 +159,17 @@ def novel_gen_from_mem(log_kind: int, novel_code: Optional[str] = None) -> tuple
     :return: Novel 객체 제너레이터, 선호작 수
     """
     mem_no: int = load_mem_no_from_env(log_kind)
-    novel_obj: Optional[Novel] = None
     if novel_code:
         novel_cnt, dic_gen = novel_dic_gen_from_mem(mem_no)
-        novel_gen = novel_gen_from_dic_gen(dic_gen)
-        novel_obj = next(novel_gen)
+        novel_obj = next(novel_gen_from_dic_gen(dic_gen))
     else:
         novel_cnt, dic_li = novel_dic_li_from_mem(mem_no)
         if novel_cnt != 1:
-            err = ValueError("잘못된 선호작 수량")
-            raise log_and_return_error(err)
-        novel_dic: dict[str, Optional[int|str]] = dic_li[-1]
-        novel_obj = novel_from_dic(novel_dic, 0)
+            raise log_and_return_error(ValueError("잘못된 선호작 수량"))
+        novel_obj = novel_from_dic(dic_li[-1], 0)
     
     if not novel_obj:
-        err = "소설 객체를 생성하지 못했어요."
-        raise ValueError(err)
+        raise ValueError("소설 객체를 생성하지 못했어요.")
     return novel_obj, novel_cnt
 
 
